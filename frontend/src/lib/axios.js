d73867c97da7633a119ee333b48250d536a74d61
Entrywayz <injector2281337@gmail.com>
2025-05-25 10:06:21 +0300

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "https://fullstack-chatapp-react-and-nodejs.onrender.com",
+    baseURL: "https://fullstack-chatapp-react-and-nodejs.onrender.com/api/",
     withCredentials: true
 })
